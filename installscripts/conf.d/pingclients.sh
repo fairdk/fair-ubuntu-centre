b21cf4ed783d5acf6fd8905607fa827854d25e80
Benjamin Bach <benjaoming@gmail.com>
2015-02-10 06:44:52 +0200

    Fix adding pingclients to rc.d with 14.04 API

--- installscripts/conf.d/pingclients.sh
+++ installscripts/conf.d/pingclients.sh
@@ -7,6 +7,5 @@ echo "Installing ping_clients.py"
 cp ${FAIR_INSTALL_DATA}/ping_clients/ping_clients.py /usr/local/sbin
 cp ${FAIR_INSTALL_DATA}/ping_clients/ping_clients /etc/init.d
 chmod +x /etc/init.d/ping_clients
-update-rc.d -f ping_clients defaults remove
+update-rc.d -f ping_clients remove
 update-rc.d ping_clients defaults
-
