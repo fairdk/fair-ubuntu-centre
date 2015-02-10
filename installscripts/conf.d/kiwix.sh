122faddc9a2fdd592e3de2ca08bb5bb5916bccc9
Benjamin Bach <benjaoming@gmail.com>
2015-02-10 22:42:55 +0200

    kiwix startup script

---
+++ installscripts/conf.d/kiwix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "---------------------------------------"
+echo "Kiwix (digital library)"
+echo "---------------------------------------"
+
+cp -Ruf $FAIR_DRIVE_MOUNTPOINT/data/kiwix/kiwix /opt/
+
+cat ${FAIR_INSTALL_DATA}/etc.init.d.kiwix > /etc/init.d/kiwix
+
+sedeasy "{{ FAIR_DRIVE_MOUNTPOINT }}" "$FAIR_DRIVE_MOUNTPOINT" /etc/init.d/kiwix
+
+chmod +x /etc/init.d/kiwix
+
+update-rc.d -f kiwix remove
+update-rc.d kiwix defaults
+
+/etc/init.d/kiwix start
