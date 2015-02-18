89462bbffe4b317d84c95d7986f98ae2466d70cd
Benjamin Bach <benjaoming@gmail.com>
2015-02-18 22:30:36 +0200

    consistent bash variable substitution

--- installscripts/conf.d/00-sources.sh
+++ installscripts/conf.d/00-sources.sh
@@ -13,8 +13,8 @@ then
 		FAIR_PARTITION_UUID=`ls -l /dev/disk/by-uuid/ | grep $FAIR_MOUNT_PARTITION | sed 's/.* \([^ ]*\) -> .*/\1/'`
 		if [[ "$FAIR_PARTITION_UUID" != "" ]]
 		then
-			echo "No entry in /etc/fstab... Will now create one for /dev/sdb1"
-			echo "UUID=$FAIR_PARTITION_UUID	${FAIR_DRIVE_MOUNTPOINT}	ext4	 errors=remount-ro,noatime,async 0 0" >> /etc/fstab
+			echo "No entry in /etc/fstab... Will now create one for UUID=${FAIR_PARTITION_UUID}"
+			echo "UUID=${FAIR_PARTITION_UUID}	${FAIR_DRIVE_MOUNTPOINT}	ext4	 errors=remount-ro,noatime,async 0 0" >> /etc/fstab
 			mkdir -p ${FAIR_DRIVE_MOUNTPOINT}
 			chmod 755 ${FAIR_DRIVE_MOUNTPOINT}
 			mount -a
