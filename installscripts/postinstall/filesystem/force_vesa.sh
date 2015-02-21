2f7d3f0aab67736a2dfe8690fc1f32a9ada192ba
benjamin bach <benjaoming@gmail.com>
2015-02-21 17:34:51 +0200

    Also remove some vesa stuff

--- installscripts/postinstall/filesystem/force_vesa.sh
+++ installscripts/postinstall/filesystem/force_vesa.sh
@@ -4,3 +4,5 @@ echo 'Section "Device"' > /usr/share/X11/xorg.conf.d/force_vesa.conf
 echo '  Identifier "Configured Video Device"' >> /usr/share/X11/xorg.conf.d/force_vesa.conf
 echo '  Driver "vesa"' >> /usr/share/X11/xorg.conf.d/force_vesa.conf
 echo 'EndSection' >> /usr/share/X11/xorg.conf.d/force_vesa.conf
+
+cp force_vesa.grub /etc/grub.d/40-force_vesa
--- installscripts/postinstall/filesystem/force_vesa_remove.sh
+++ installscripts/postinstall/filesystem/force_vesa_remove.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm /usr/share/X11/xorg.conf.d/force_vesa.conf
+rm /etc/grub.d/40-force_vesa
 echo "Removed force_vesa.conf"
