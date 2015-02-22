84e3519ce4b442641a9a53dc760d0bad1a3a93da
Benjamin Bach <benjaoming@gmail.com>
2015-02-23 00:55:33 +0200

    Removing issue when no UUID or multiple FAIR drives

--- installscripts/conf.d/00-sources.sh
+++ installscripts/conf.d/00-sources.sh
@@ -7,20 +7,30 @@ echo "---------------------------------------"
 if (("$USE_FAIR_DISK"==1))
 then
 	sedeasy_delete "${FAIR_DRIVE_MOUNTPOINT}" /etc/fstab
+	echo "Removed ${FAIR_DRIVE_MOUNTPOINT} from fstab"
+	if mount | grep "${FAIR_DRIVE_MOUNTPOINT}"
+	then
+		echo "Unmounting ${FAIR_DRIVE_MOUNTPOINT}"
+		umount "${FAIR_DRIVE_MOUNTPOINT}"
+	fi
+	# TODO: This should not be necessary anymore because above should delete
+	# drive in all cases.
 	if ! grep "${FAIR_DRIVE_MOUNTPOINT}" /etc/fstab -q
 	then
-		FAIR_MOUNT_PARTITION=`mount |grep FAIR|sed 's/\/dev\/\(....\)\ .*/\1/'`
+		FAIR_MOUNT_PARTITION=`mount | grep FAIR | sed 's/\/dev\/\(....\)\ .*/\1/g'`
 		FAIR_PARTITION_UUID=`ls -l /dev/disk/by-uuid/ | grep $FAIR_MOUNT_PARTITION | sed 's/.* \([^ ]*\) -> .*/\1/'`
-		if [[ "$FAIR_PARTITION_UUID" != "" ]]
+		if [[ "$FAIR_PARTITION_UUID" == "" ]]
 		then
-			echo "No entry in /etc/fstab... Will now create one for UUID=${FAIR_PARTITION_UUID}"
-			echo "UUID=${FAIR_PARTITION_UUID}	${FAIR_DRIVE_MOUNTPOINT}	ext4	 errors=remount-ro,noatime,async 0 0" >> /etc/fstab
-			mkdir -p ${FAIR_DRIVE_MOUNTPOINT}
-			chmod 755 ${FAIR_DRIVE_MOUNTPOINT}
-			mount -a
+			FAIR_PARTITION_UUID="/dev/$FAIR_MOUNT_PARTITION"
+			echo "WARNING! No UUID found for partition, will use unreliable drive letter"
 		else
-			echo "No UUID for FAIR archive path, assuming it's a directory"
+			FAIR_PARTITION_UUID="UUID=${FAIR_PARTITION_UUID}"
 		fi
+		echo "No entry in /etc/fstab... Will now create one for ${FAIR_PARTITION_UUID}"
+		echo "${FAIR_PARTITION_UUID}	${FAIR_DRIVE_MOUNTPOINT}	ext4	 errors=remount-ro,noatime,async 0 0" >> /etc/fstab
+		mkdir -p ${FAIR_DRIVE_MOUNTPOINT}
+		chmod 755 ${FAIR_DRIVE_MOUNTPOINT}
+		mount -a
 	else
 		echo "Already present!"
 	fi
