ca3c345bfca2efec49e074b388ee97b04d33c3ee
Benjamin Bach <benjaoming@gmail.com>
2015-02-23 08:43:31 +0200

    fix race-condition when network-manager tries to access eth0 before it has been made unmanaged

--- installscripts/conf.d/02-network.sh
+++ installscripts/conf.d/02-network.sh
@@ -9,13 +9,15 @@ echo "---------------------------------------"
 # The network manager conflicts with the dnsmasq package, so if we want to keep it (and I do for development) we have to reconfigure it
 # see also: http://sokratisg.net/2012/03/31/ubuntu-precise-12-04-get-rid-of-nms-dnsmasq-and-setup-your-own/
 cat ${FAIR_INSTALL_DATA}/etc.NetworkManager.conf > /etc/NetworkManager/NetworkManager.conf
-restart network-manager
+stop network-manager
 sleep 5
 
 #PDO: The ifup command fails on my development system, maybe because I'm still using the network manager.
 cat ${FAIR_INSTALL_DATA}/etc.network.interfaces > /etc/network/interfaces
 ifdown -a
 ifup -a
+
+start network-manager
 
 echo "Altering /etc/hosts"
 # Here we add a number of hostnames to the 'hosts' file, so we can resolve the various alaises we have for the FAIR server.  Note that the IP address is fixed in the /etc/network/interfaces.
