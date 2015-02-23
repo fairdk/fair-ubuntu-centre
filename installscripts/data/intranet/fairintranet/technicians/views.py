4078432a4a0286af8f141eeb2ee91a2f3ffba6d2
Benjamin Bach <benjaoming@gmail.com>
2015-02-23 11:49:49 +0200

    new view to register computers

--- installscripts/data/intranet/fairintranet/technicians/views.py
+++ installscripts/data/intranet/fairintranet/technicians/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from . import models
+
+def register_computer_installation(request, computer_label=None):
+
+    computer, created = models.Computer.objects.get_or_create(label=computer_label)
+    computer.installed = timezone.now()
+    computer.save()
+
+    return HttpResponse("OK")
