3b5f16e4ddb353f1bdbcdc54835eb7af65e488d1
Benjamin Bach <benjamin@overtag.dk>
2017-03-07 15:34:56 +0200

    Remove KA Lite local_settings (deprecated)

--- installscripts/conf.d/kalite.sh
+++ installscripts/conf.d/kalite.sh
@@ -24,9 +24,6 @@ DEBIAN_FRONTEND=noninteractive dpkg -i $FAIR_DRIVE_MOUNTPOINT/data/ka-lite/ka-li
 
 cat ${FAIR_INSTALL_DATA}/etc.apache2.sites-available.kalite.conf > /etc/apache2/sites-available/kalite.conf
 
-# Patch local_settings to use db based sessions
-cat ${FAIR_INSTALL_DATA}/kalite_local_settings.py > $FAIR_DRIVE_MOUNTPOINT/data/ka-lite/kalite/local_settings.py
-
 # Patch errors in ka lite imports
 echo "" > $FAIR_DRIVE_MOUNTPOINT/data/ka-lite/kalite/testing/__init__.py
 echo "" > $FAIR_DRIVE_MOUNTPOINT/data/ka-lite/python-packages/fle_utils/testing/__init__.py
